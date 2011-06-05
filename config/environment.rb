2d8cb9bca285c6a6f742c168bbbd0545254df015
Mike Shea <mike@mikeshea.net>
2011-06-05 08:54:08 -0400

    working build

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,6 @@ require File.expand_path('../application', __FILE__)
 require 'open-uri'
 require 'csv'
 require 'time'
-#require 'twitter'
+
 # Initialize the rails application
 FetchFavorites::Application.initialize!
